7704b3eeff65dcee78c1d5592b6bd5513d882888
CreaSoley <artcreatifmeg@gmail.com>
2025-11-16 09:21:17 +0100

    Add global URL variable for GAS techniques
    Added global variable for Google Apps Script URL.

---
+++ js/techniques-aleatoires.js
@@ -0,0 +1,4 @@
+// Variable globale pour l'URL de votre script Google Apps
+const URL_GAS_TECHNIQUES = "https://script.google.com/macros/s/AKfycbwfuW9f0s7Q0ckX6qjgXrMWyDjrQGzw4hlPOLVcVwEliMh1vWEqTgzqeBWZgC-pyjpF/exec"; 
+
+// ... [Collez ici la fonction choisirTechnique() et le reste du code défini précédemment] ...
