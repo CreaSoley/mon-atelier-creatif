2ae9dfd1d1bbcdf07c6ff8cb8ac7b338ab142cfc
CreaSoley <artcreatifmeg@gmail.com>
2025-11-16 09:23:21 +0100

    Add initial structure for poetic weather functions

---
+++ js/meteo-poetique.js
@@ -0,0 +1,8 @@
+// --- Météo Poétique ---
+function getPoeticWeather(weatherCode) {
+    // ... [Collez ici la fonction getPoeticWeather()] ...
+}
+function chargerMeteoPoetique() {
+    // ... [Collez ici la fonction chargerMeteoPoetique()] ...
+}
+document.addEventListener('DOMContentLoaded', chargerMeteoPoetique);
